614d4ae0afd32090092ed64cd846037d2d49f0e5
Vedang Manerikar <ved.manerikar@gmail.com>
2014-08-30 14:52:07 +0530

    Bring 'saythanks' up-to-date
    Upgrade all the libs.

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
-(defproject saythanks "0.1.0-SNAPSHOT"
+(defproject saythanks "0.2.0"
   :description "Say thanks to all the wonderful people who wish you on
                your birthday."
   :url "http://vedang.me/techlog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.4.3"]
-                 [clj-time "0.4.3"]
-                 [carmine "0.8.2-SNAPSHOT"]
-                 [org.clojure/data.json "0.1.3"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.0"]
+                 [clj-time "0.8.0"]
+                 [com.taoensso/carmine "2.6.0"]
+                 [org.clojure/data.json "0.2.5"
                   :exclusions [org.clojure/clojure]]]
-  :min-lein-version "2.0.0"
-  :warn-on-reflection true
+  :min-lein-version "2.4.3"
+  :global-vars {*warn-on-reflection* true}
+  :aot [saythanks.core]
   :main saythanks.core)
