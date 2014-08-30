b2a566154f324670d66d771b79ce97b3bb9296e9
Vedang Manerikar <ved.manerikar@gmail.com>
2014-08-30 14:52:51 +0530

    Modify redis wrapper to work with new carmine version

--- src/saythanks/redis.clj
+++ src/saythanks/redis.clj
@@ -1,26 +1,22 @@
 (ns ^{:doc "Settings and other functions for Redis"
       :author "Vedang Manerikar <vedang@helpshift.com>"}
   saythanks.redis
-  (:require [carmine.core :as r]))
+  (:require [taoensso.carmine :refer [wcar]]))
 
 
-(defonce ^{:doc "The global Redis pool."} pool nil)
-(defonce ^{:doc "Server spec for Redis connection"} server-spec nil)
+(defonce ^{:doc "Server connection for Redis"} server-conn (atom nil))
 
 (defn init-redis!
   "Initialize Redis connection"
   {:arglists '([{:keys [host port password timeout]}])}
-  [server-spec]
-  (let [redis-pool (r/make-conn-pool :max-active 8)
-        redis-server-spec (apply r/make-conn-spec
-                                 (apply concat server-spec))]
-    (alter-var-root #'pool (constantly redis-pool))
-    (alter-var-root #'server-spec (constantly redis-server-spec))))
+  [redis-server-spec]
+  (let [redis-server-conn {:pool {:max-active 8}
+                           :spec redis-server-spec}]
+    (reset! server-conn redis-server-conn)))
 
 
 (defmacro redis
-  "Basically like (partial with-conn pool spec-server1).
-  Taken from Carmine documentation:
-  https://github.com/ptaoussanis/carmine"
-  [& body]
-  `(r/with-conn pool server-spec ~@body))
+  [& [a1 & an :as args]]
+  (if (symbol? a1)
+    `(wcar (deref ~a1) ~@an)
+    `(wcar (deref server-conn) ~@args)))
